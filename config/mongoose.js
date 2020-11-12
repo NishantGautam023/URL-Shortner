e2ef43166ab23c34c6f0c75dc5361f740cec60c9
Nishant Gautam <eddienishant@gmail.com>
2020-11-12 19:18:40 +0530

    building schema

---
+++ config/mongoose.js
@@ -0,0 +1,6 @@
+// Model that stores all the shortUrls information 
+/* https://mongoosejs.com/docs/guide.html */
+
+
+
+
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const app = express();
 const port = 8000;
+const app = express();
+
+/* ******* setting up our database    https://mongoosejs.com/docs/ *********  */
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/urlShortneer', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+    // The above 2 statements helps us to avoid  the Deprecation Warning !!!
+})
 
 // ***********Setting up our view engine********
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
 // ********* static files and parsing the form data
-app.use(express.urlencoded());
+app.use(express.urlencoded({
+    extended: true // helps us to avoid body-parser deprecated warning
+}));
+
 app.use(express.static('assets')) // only on require we use './'
 
 
