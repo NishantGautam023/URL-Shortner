6538a21bc871d05f4877e30cb217247dbf23ebe7
Nishant Gautam <eddienishant@gmail.com>
2020-11-12 09:24:45 +0530

    Server is running

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+
+
+app.listen(port, (err) => {
+    if(err) {
+        console.log(`There is an error ${err}`)
+        return;
+    }
+    console.log(`Our server is running at port ${port}`)
+})
